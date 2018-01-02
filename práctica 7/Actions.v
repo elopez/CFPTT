30b212f8bc43282c8d000efcd62019abb0d05a1a
Emilio López <elopez93@gmail.com>
2018-01-02 18:16:46 -0300

    Ejercicio 7.2

--- práctica 7/Actions.v
+++ práctica 7/Actions.v
@@ -3,3 +3,18 @@
  * (Como transformadores de estado)
  ******************************************************************)
 
+Require Export State.
+
+Section Actions.
+
+Inductive Action :=
+  | read : vadd -> Action
+  | write : vadd -> value -> Action
+  | chmod : Action
+.
+
+Definition Pre := state -> Action -> Prop.
+Definition Post := state -> Action -> state -> Prop.
+
+
+End Actions.
